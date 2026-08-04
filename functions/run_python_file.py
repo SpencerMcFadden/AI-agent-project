91427ddd54ae0d27de8e63383cc68696427b7262
SpencerMcFadden <mcfadden.spencer@protonmail.com>
2026-08-04 10:45:44 -0400

    adds python running function and tests

---
+++ functions/run_python_file.py
@@ -0,0 +1,30 @@
+import os.path
+import subprocess
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(abs_file_path):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not abs_file_path.endswith('.py'):
+            return f'Error: "{file_path}" is not a Python file'
+        command = ["python", abs_file_path]
+        if args:
+            command.extend(args)
+        subprocess_result = subprocess.run(command, cwd=abs_working_dir, capture_output=True, text=True, timeout=30)
+        return_str = ''
+        if subprocess_result.returncode != 0:
+            return_str += f'Process exited with code {subprocess_result.returncode}'
+        if not subprocess_result.stdout and not subprocess_result.stderr:
+            return_str += 'No output produced'
+        else:
+            return_str += f'STDOUT: {subprocess_result.stdout}'
+            return_str += f'STDERR: {subprocess_result.stderr}'
+        return return_str
+    except Exception as e:
+        raise OSError(e)
